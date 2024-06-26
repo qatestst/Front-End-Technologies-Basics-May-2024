e7f94311dc460ea770eeb31331102ca7e22fe26f
user <user>
2024-06-26 16:51:56 +0300

    new

--- 21.Exam-preparation-2/20-Exam-Preparation-II-Resources/Task-2 - QUnit and Playwright/tests/QUnit_tests/integration.test.js
+++ 21.Exam-preparation-2/20-Exam-Preparation-II-Resources/Task-2 - QUnit and Playwright/tests/QUnit_tests/integration.test.js
@@ -18,6 +18,8 @@ let eventData = {
         imageUrl: "/images/2.png"
 }
 
+let lastCreatedId;
+let lastEditedId;
 
 QUnit.module('Test user functionalities', ()=>{
     QUnit.test('Register user with valid data', async (assert) =>{
@@ -172,6 +174,8 @@ QUnit.module('Test Event functionality', ()=>{
             assert.ok(json.hasOwnProperty('author'), 'Event has property "author"');
             assert.strictEqual(typeof json['author'], 'string', 'Event property "author is string"');
             assert.equal(json['author'], eventData.author, 'Expected author');
+            // the same is the same
+            //assert.equal(json.author, eventData.author, 'Expected author');
 
             assert.ok(json.hasOwnProperty('date'), 'Event has property "date"');
             assert.strictEqual(typeof json['date'], 'string', 'Event property "date is string"');
@@ -187,7 +191,8 @@ QUnit.module('Test Event functionality', ()=>{
 
             assert.ok(json.hasOwnProperty('title'), 'Event has property "title"');
             assert.strictEqual(typeof json['title'], 'string', 'Event property "title" is string"');
-            assert.equal(json['title'], eventData.title, 'Expected title');
+            assert.equal(json['title'], eventData.title, 'Expected title'); // the same as: assert.equal(json.title, eventData.title, 'Expected title');
+            
 
             assert.ok(json.hasOwnProperty('_createdOn'), 'Event has property "_createdOn"');
             assert.strictEqual(typeof json['_createdOn'], 'number', 'Event property "_createdOn" is number"');
@@ -198,16 +203,79 @@ QUnit.module('Test Event functionality', ()=>{
             assert.ok(json.hasOwnProperty('_ownerId'), 'Event has property "_ownerId"');
             assert.strictEqual(typeof json['_ownerId'], 'string', 'Event property "_ownerId" is string"');
 
-                   
+            lastCreatedId = json._id;
+             
     });
 
-    // QUnit.test('Edit Event', async (assert)=>{
+    QUnit.test('Edit Event', async (assert)=>{
+        //arrange
+        let path = 'data/theaters';
+        let random = Math.floor(Math.random() * 10000);
 
-    // });
+        eventData.title =`Edited Random title ${random}`;
 
-    // QUnit.test('Delete Event', async (assert)=>{
+        //act
+        let response = await fetch(baseUrl + path +`/${lastCreatedId}`, {
+            method: 'PUT',
+            headers: {
+                'content-type' : 'application/json',
+                'X-Authorization' : token
+            },
+            body: JSON.stringify(eventData)
+        });
 
-    // });
+        let json = await response.json();
+        // assert
+        assert.ok(json.hasOwnProperty('author'), 'Event has property "author"');
+            assert.strictEqual(typeof json['author'], 'string', 'Event property "author is string"');
+            assert.equal(json['author'], eventData.author, 'Expected author');
+            // the same is the same
+            //assert.equal(json.author, eventData.author, 'Expected author');
+
+            assert.ok(json.hasOwnProperty('date'), 'Event has property "date"');
+            assert.strictEqual(typeof json['date'], 'string', 'Event property "date is string"');
+            assert.equal(json['date'], eventData.date, 'Expected date');
+
+            assert.ok(json.hasOwnProperty('description'), 'Event has property "description"');
+            assert.strictEqual(typeof json['description'], 'string', 'Event property "description is string"');
+            assert.equal(json['description'], eventData.description, 'Expected description');
+
+            assert.ok(json.hasOwnProperty('imageUrl'), 'Event has property "imageUrl"');
+            assert.strictEqual(typeof json['imageUrl'], 'string', 'Event property "imageUrl" is string"');
+            assert.equal(json['imageUrl'], eventData.imageUrl, 'Expected imageUrl');
+
+            assert.ok(json.hasOwnProperty('title'), 'Event has property "title"');
+            assert.strictEqual(typeof json['title'], 'string', 'Event property "title" is string"');
+            assert.equal(json['title'], eventData.title, 'Expected title'); // the same as: assert.equal(json.title, eventData.title, 'Expected title');
+            
+
+            assert.ok(json.hasOwnProperty('_createdOn'), 'Event has property "_createdOn"');
+            assert.strictEqual(typeof json['_createdOn'], 'number', 'Event property "_createdOn" is number"');
+
+            assert.ok(json.hasOwnProperty('_id'), 'Event has property "_id"');
+            assert.strictEqual(typeof json['_id'], 'string', 'Event property "_id" is string"');
+
+            assert.ok(json.hasOwnProperty('_ownerId'), 'Event has property "_ownerId"');
+            assert.strictEqual(typeof json['_ownerId'], 'string', 'Event property "_ownerId" is string"');
+
+            lastEditedId = json._id;
+
+    });
+
+    QUnit.test('Delete Event', async (assert)=>{
+        let path = 'data/theaters';
+
+        let response = await fetch(baseUrl + path + `/${lastEditedId}`, {
+            method: 'DELETE',
+            headers: {
+                'X-Authorization' : token
+            }
+        });
+
+        //assert
+        assert.ok(response.ok)
+
+    });
 
 
 
