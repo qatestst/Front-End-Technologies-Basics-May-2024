4ef6f876956dc35d6e0133db62df78227e6bbab0
qatestst <134833768+qatestst@users.noreply.github.com>
2024-06-28 18:44:21 +0300

    new

--- 21.Exam-preparation-2/20-Exam-Preparation-II-Resources/Task-2 - QUnit and Playwright/tests/Playwright_tests/e2e.test.js
+++ 21.Exam-preparation-2/20-Exam-Preparation-II-Resources/Task-2 - QUnit and Playwright/tests/Playwright_tests/e2e.test.js
@@ -13,6 +13,8 @@ let user = {
     confirmPass : "123456",
 };
 
+let token;
+
 describe("e2e tests", () => {
     beforeAll(async () => {
         browser = await chromium.launch();
@@ -33,15 +35,209 @@ describe("e2e tests", () => {
     });
 
     
+
     describe("authentication", () => {
-        
+        test("register makes correct api call", async () => {
+            //arrange
+            await page.goto(host);
+            await page.click("text=Register");
+            await page.waitForSelector('form');
+            let random = Math.floor(Math.random()*10000);
+            user.email = `abv${random}@abv.bg`;
+
+            //act
+            await page.locator('#email').fill(user.email);
+            await page.locator('#password').fill(user.password);
+            await page.locator('#repeatPassword').fill(user.confirmPass);
+            let [response] = await Promise.all([
+                page.waitForResponse(response => response.url().includes("/users/register") && response.status() == 200),
+                page.click('[type="submit"]')
+            ])
+            let userData = await response.json();
+
+            //assert
+            await expect(response.ok()).toBeTruthy();
+            expect(userData.email).toBe(user.email);
+            expect(userData.password).toBe(user.password);
+        });
+
+        test("Login makes correct API call ", async () =>{
+            //arrange
+            await page.goto(host);
+            await page.click("text=Login");
+            await page.waitForSelector('form');
+            
+            //act
+            await page.locator('#email').fill(user.email);
+            await page.locator('#password').fill(user.password);
+
+
+            let [response] = await Promise.all([
+                page.waitForResponse(response => response.url().includes("/users/login") && response.status() == 200),
+                page.click('[type="submit"]')
+            ])
+            let userData = await response.json();
+            console.log(userData);
+
+            //assert
+            await expect(response.ok()).toBeTruthy();
+            expect(userData.email).toBe(user.email);
+            expect(userData.password).toBe(user.password);
+
+            token = userData.accessToken;
+                        
+        });
+
+        test("Logout makes correct API call ", async () =>{
+            //arrange
+            await page.goto(host);
+            await page.click("text=Login");
+            await page.waitForSelector('form');
+            await page.locator('#email').fill(user.email);
+            await page.locator('#password').fill(user.password);
+            await page.click('[type="submit"]');
+
+            let [response] = await Promise.all([
+                page.waitForResponse(response => response.url().includes("/users/logout") && response.status() == 204),
+                page.click('nav >> text=Logout')
+            ])
+
+            //assert
+            await expect(response.ok()).toBeTruthy();
+            await page.waitForSelector('nav >> text=Login')
+            await expect(page.url()).toBe(host + '/');     
+                                   
+        })
+
     });
 
     describe("navbar", () => {
-        
+        test('Logged user should see correct navigation buttons',async () =>{
+            //arrange & //act
+            await page.goto(host);
+            await page.click("text=Login");
+            await page.waitForSelector('form');
+            await page.locator('#email').fill(user.email);
+            await page.locator('#password').fill(user.password);
+            await page.click('[type="submit"]');
+
+            //assert
+            await expect(page.locator('nav >> text=Theater')).toBeVisible();
+            await expect(page.locator('nav >> text=Profile')).toBeVisible();
+            await expect(page.locator('nav >> text=Create Event')).toBeVisible();
+            await expect(page.locator('nav >> text=Logout')).toBeVisible();
+            await expect(page.locator('nav >> text=Login')).toBeHidden();
+            await expect(page.locator('nav >> text=Register')).toBeHidden();
+        })
+
+        test('guest user should see correct navigation buttons', async ()=>{
+             //arrange & //act
+             await page.goto(host);
+              
+             //assert
+             await expect(page.locator('nav >> text=Login')).toBeVisible();
+             await expect(page.locator('nav >> text=Register')).toBeVisible();
+             await expect(page.locator('nav >> text=Theater')).toBeVisible();
+
+             await expect(page.locator('nav >> text=Profile')).toBeHidden();
+             await expect(page.locator('nav >> text=Create Event')).toBeHidden();
+             await expect(page.locator('nav >> text=Logout')).toBeHidden();
+                        
+        });
+
     });
 
     describe("CRUD", () => {
+        beforeEach(async () =>{
+            await page.goto(host);
+            await page.click("text=Login");
+            await page.waitForSelector('form');
+            await page.locator('#email').fill(user.email);
+            await page.locator('#password').fill(user.password);
+            await page.click('[type="submit"]');
+
+        })
+
+        test('Create makes correct API call', async ()=> {
+           await page.click('nav >> text=Create Event');
+           await page.waitForSelector('form');
+
+           await page.fill('#title', 'Random title');
+           await page.fill('#date', 'Random 11/11/2024');
+           await page.fill('#author', 'Random author');
+           await page.fill('#description', 'Random description');
+           await page.fill('#imageUrl', '/images/backgroung.png');
+
+           
+           let [response] = await Promise.all([
+                page.waitForResponse(response => response.url().includes("/data/theaters") && response.status() == 200),
+                page.click('[type="submit"]')
+            ]);
+
+            await expect(response.ok()).toBeTruthy();
+            let responseData = await response.json();
+            console.log(responseData);
+
+            //expect(response).toBe(user.password);
+            await expect(responseData.author).toEqual('Random author');
+            await expect(responseData.title).toEqual('Random title');
+            await expect(responseData.imageUrl).toEqual('/images/backgroung.png');
+            await expect(responseData.description).toEqual('Random description');
+            await expect(responseData.date).toEqual('Random 11/11/2024');
+                    
+        })
+
+        test('Edit an event', async() =>{
+           await page.click('nav >> text=Profile');
+           await page.waitForSelector('text=Details');
+           await page.locator('text=Details').first().click();
+           //await page.click('text=Details');
+           await page.waitForSelector('text=Edit');
+           await page.click('text=Edit');
+           await page.waitForSelector('form');
+
+
+           await page.fill('#title', 'Edited title');
+           await page.fill('#date', 'Edited date');
+           await page.fill('#author', 'Edited author');
+           await page.fill('#description', 'Edited description');
+           await page.fill('#imageUrl', 'Edited imageUrl');
+
+
+
+           let [response] = await Promise.all([
+            page.waitForResponse(response => response.url().includes("/data/theaters") && response.status() == 200),
+            page.click('[type="submit"]')
+            ]);
+
+            await expect(response.ok()).toBeTruthy();
+            let responseData = await response.json();
+            console.log(responseData);
+
+            //expect(response).toBe(user.password);
+            await expect(responseData.author).toEqual('Edited author');
+            await expect(responseData.title).toEqual('Edited title');
+            await expect(responseData.imageUrl).toEqual('Edited imageUrl');
+            await expect(responseData.description).toEqual('Edited description');
+            await expect(responseData.date).toEqual('Edited date');
+
+
+        });
+
+        test('Delete an event', async ()=>{
+           await page.click('nav >> text=Profile');
+           await page.locator('text=Details').first().click();
+            
+            let [response] = await Promise.all([
+                page.waitForResponse(response => response.url().includes("/data/theaters") && response.status() == 200),
+                page.on('dialog', dialog => dialog.accept()),
+                page.click('text=Delete')
+            ]);
+    
+            await expect(response.ok()).toBeTruthy();
+            let responseData = await response.json();
+            console.log(responseData);
+        });
         
     })
 })
--- 22.Exam-preparation-3/21-Exam-Preparation-III-Resources/Task-1/app.js
+++ 22.Exam-preparation-3/21-Exam-Preparation-III-Resources/Task-1/app.js
@@ -2,8 +2,153 @@ window.addEventListener('load', solve);
 
 function solve() {
 
+//initial map elements
+
+const timeElement = document.getElementById('time');
+const dateElement = document.getElementById('date');
+const placeElement = document.getElementById('place');
+const eventElement = document.getElementById('event-name');
+const emailElement = document.getElementById('email');
+
+const addButtonElement = document.getElementById('add-btn');
+
+const checkListElement = document.getElementById('check-list');
+const upcomingListElement = document.getElementById('upcoming-list');
+const finishedListElement = document.getElementById('finished-list');
+
+let clearButtonElement = document.getElementById('clear')
+
+//add event Listener fort Add event Button
+addButtonElement.addEventListener("click", onNext);
+
+function onNext(e){
+    e.preventDefault();
+    if(timeElement.value == '' ||
+       dateElement.value == '' ||
+       placeElement.value == '' ||
+       eventElement.value == '' ||
+       emailElement.value == ''
+    ){
+        return;
+    }
+
+    //building li element
+
+    let liElement = document.createElement('li');
+    liElement.setAttribute('class', "event-content");
+
+    let articleElement = document.createElement('article');
+
+    let timeParagraph = document.createElement('p');
+    timeParagraph.textContent = `Begins: ${dateElement.value} at: ${timeElement.value}`
+
+    let placeParagraph = document.createElement('p');
+    placeParagraph.textContent = `In: ${placeElement.value}`;
+
+    let eventParagraph = document.createElement('p');
+    eventParagraph.textContent = `Event: ${eventElement.value}`;
+
+    let emailParagraph = document.createElement('p');
+    emailParagraph.textContent = `Contact: ${emailElement.value}`
+
+    let editBtn = document.createElement('button');
+    editBtn.setAttribute('class', 'edit-btn');
+    editBtn.textContent = 'Edit';
+
+    let continueBtn = document.createElement('button');
+    continueBtn.setAttribute('class', 'continue-btn');
+    continueBtn.textContent = 'Continue';
+
+
+    articleElement.appendChild(timeParagraph);
+    articleElement.appendChild(placeParagraph);
+    articleElement.appendChild(eventParagraph);
+    articleElement.appendChild(emailParagraph);
+
+    liElement.appendChild(articleElement);
+    liElement.appendChild(editBtn);
+    liElement.appendChild(continueBtn);
+    
+    checkListElement.appendChild(liElement);
+
+    addButtonElement.disabled = true;
+
+
+    //save data from elemnts before Edit
+
+    let editedTimeElement = timeElement.value;
+    let editedDataElement = dateElement.value;
+    let editedPlaceElement = placeElement.value;
+    let editedEventElement = eventElement.value;
+    let editedEmailElement = emailElement.value;
+
+    timeElement.value = '';
+    dateElement.value = '';
+    placeElement.value = '';
+    eventElement.value = '';
+    emailElement.value = '';   
+
+
+    editBtn.addEventListener('click', onEdit);
+
+    function onEdit(){
+        timeElement.value = editedTimeElement;
+        dateElement.value = editedDataElement;
+        placeElement.value = editedPlaceElement;
+        eventElement.value = editedEventElement;
+        emailElement.value = editedEmailElement;
+
+        liElement.remove();
+        addButtonElement.disabled = false;
+
+    }
+
+    continueBtn.addEventListener('click', onContinue);
+
+    function onContinue() {
+        let liElementContinue = document.createElement('li');
+        let articleElementContinue = document.createElement('article');
+        articleElementContinue = articleElement;
+        
+
+        let moveToFinishedBtn = document.createElement('button');
+        moveToFinishedBtn.setAttribute('class', 'finished-btn');
+        moveToFinishedBtn.textContent = 'Move to Finished'
+        
+        
+        liElementContinue.appendChild(articleElementContinue);
+        liElementContinue.appendChild(moveToFinishedBtn);
+
+        liElement.remove();
+
+        upcomingListElement.appendChild(liElementContinue);
+
+        moveToFinishedBtn.addEventListener('click', moveToFinished);
+        moveToFinishedBtn.setAttribute('class', 'finished-btn');
+        moveToFinishedBtn.textContent = 'Move to Finished';
+        
+        addButtonElement.disabled = false;
+
+        function moveToFinished(){
+            let liFinishedElement = document.createElement('li');
+            liFinishedElement.setAttribute('class', 'event-content');
+
+            let articleFinishedElement = document.createElement('article');
+            articleFinishedElement = articleElementContinue;
+    
+            liFinishedElement.appendChild(articleFinishedElement);
+            finishedListElement.appendChild(liFinishedElement);
+
+            liElementContinue.remove();
+
+            // add logic for clear button                               
+            clearButtonElement.addEventListener('click', onClear);
+
+            function onClear() {
+                liFinishedElement.remove();
+            }
+                           
+        }
+    }
+   }
 }
-
-
-    
-    
